66270d29901e10e0a16a41a7ea9553dd01ad69f6
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-02-18 19:38:02 +0100

    Update Buch.java

--- Programmtext/Bibliothek_in_Java/Buch.java
+++ Programmtext/Bibliothek_in_Java/Buch.java
@@ -1,30 +1,19 @@
 public class Buch extends Gegenstand {
     int jahr;
-    int ISBN;
+    int isbn;
     String genre;
     String author;
-
-    public Buch(int pYear, int pISBN, String pGenre, String pAuthor, int pId, String pTitel) {
+    
+    public Buch(int pJahr, int pISBN, String pGenre, String pAuthor, int pId, String pTitel) {
         super(pId, pTitel);
-        jahr = pYear;
-        ISBN = pISBN;
+        jahr = pJahr;
+        isbn = pISBN;
         genre = pGenre;
         author = pAuthor;
     }
 
-    public int getJahr() {
-        return jahr;
-    }
-
-    public int getISBN() {
-        return ISBN;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
+    public int getJahr() { return jahr; }
+    public int getISBN() { return isbn; }
+    public String getGenre() { return genre; }
+    public String getAuthor() { return author; }
 }
